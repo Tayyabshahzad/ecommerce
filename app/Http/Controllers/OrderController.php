c834fedbea5f713fcc734771b7f69e2b6389270a
Tayyab Shahzad Janjua <arhamsaeed294@gmail.com>
2024-03-29 03:16:18 +0500

    dd

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -257,7 +257,7 @@ class OrderController extends Controller
 
     public function productTrackOrder(Request $request){
         // return $request->all();
-        $order=Order::where('user_id',auth()->user()->id)->where('order_number',$request->order_number)->first();
+        $order=Order::where('order_number',$request->order_number)->first();
         if($order){
             if($order->status=="new"){
                 return redirect()->back()->with('success','Your order has been placed. please wait.');
